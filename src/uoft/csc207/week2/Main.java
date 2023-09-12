483cc98a357524b917e9730b2e4ffa6d35720c32
lisa <lis.ye@mail.utoronto.ca>
2023-09-12 13:34:20 -0400

    testing grrrr.

--- src/uoft/csc207/week2/Main.java
+++ src/uoft/csc207/week2/Main.java
@@ -2,6 +2,7 @@ package uoft.csc207.week2;
 
 public class Main {
 
+    // HEHEHE
     public static void main(String[] args) {
         String[] name = {"First", "Middle", "Last"};
 	    Person p = new Person(name, "moogah");
